d4fde8bb4600521beea5ce643ee243857e696bf6
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-14 11:05:45 -0300

    Category

--- src/main/java/com/tecsoluction/course/config/TestConifg.java
+++ src/main/java/com/tecsoluction/course/config/TestConifg.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.tecsoluction.course.entities.Category;
 import com.tecsoluction.course.entities.Order;
 import com.tecsoluction.course.entities.User;
 import com.tecsoluction.course.entities.enums.OrderStatus;
+import com.tecsoluction.course.repositories.CategoryRepository;
 import com.tecsoluction.course.repositories.OrderRepository;
 import com.tecsoluction.course.repositories.UserRepository;
 
@@ -23,8 +25,17 @@ public class TestConifg implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
