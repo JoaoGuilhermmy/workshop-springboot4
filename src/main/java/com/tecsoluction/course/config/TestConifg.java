c7308fe7853436bfbfa633068521b5a14a473f69
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-15 10:32:27 -0300

    OrderItem, many-to-many association with extra attributes parte 2

--- src/main/java/com/tecsoluction/course/config/TestConifg.java
+++ src/main/java/com/tecsoluction/course/config/TestConifg.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.tecsoluction.course.entities.Category;
 import com.tecsoluction.course.entities.Order;
+import com.tecsoluction.course.entities.OrderItem;
 import com.tecsoluction.course.entities.Product;
 import com.tecsoluction.course.entities.User;
 import com.tecsoluction.course.entities.enums.OrderStatus;
 import com.tecsoluction.course.repositories.CategoryRepository;
+import com.tecsoluction.course.repositories.OrdemItemRepository;
 import com.tecsoluction.course.repositories.OrderRepository;
 import com.tecsoluction.course.repositories.ProductRepository;
 import com.tecsoluction.course.repositories.UserRepository;
@@ -32,6 +34,9 @@ public class TestConifg implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrdemItemRepository ordemItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -66,6 +71,13 @@ public class TestConifg implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 
 }
--- src/main/java/com/tecsoluction/course/entities/OrderItem.java
+++ src/main/java/com/tecsoluction/course/entities/OrderItem.java
@@ -2,6 +2,7 @@ package com.tecsoluction.course.entities;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tecsoluction.course.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
@@ -14,7 +15,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -30,16 +31,13 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
 
     public void setOrder(Order order) {
         id.setOrder(order);
-    }
-
-    public Product geProduct() {
-        return id.getProduct();
     }
 
     public void setProduct(Product product) {
@@ -60,6 +58,10 @@ public class OrderItem implements Serializable {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
     }
 
     @Override
