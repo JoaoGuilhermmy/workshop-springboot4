366516b7379de332c92b62d0d77e0d20e9638e49
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-15 19:08:35 -0300

    User insert

--- src/main/java/com/tecsoluction/course/entities/OrderItem.java
+++ src/main/java/com/tecsoluction/course/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
         return id.getProduct();
     }
 
+    public Double getSubTotal() {
+        return price * quantity;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
