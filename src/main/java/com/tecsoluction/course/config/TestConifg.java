086b9ee430f3fab98efcd0c2cf83ec58b413c20b
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-15 11:12:00 -0300

    Payment, one-to-one association

--- src/main/java/com/tecsoluction/course/config/TestConifg.java
+++ src/main/java/com/tecsoluction/course/config/TestConifg.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.tecsoluction.course.entities.Category;
 import com.tecsoluction.course.entities.Order;
 import com.tecsoluction.course.entities.OrderItem;
+import com.tecsoluction.course.entities.Payment;
 import com.tecsoluction.course.entities.Product;
 import com.tecsoluction.course.entities.User;
 import com.tecsoluction.course.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConifg implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o3);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
