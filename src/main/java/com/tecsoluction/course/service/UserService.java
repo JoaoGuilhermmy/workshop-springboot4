86e290b5f5d3a48dfccf74487fd36f53dae6ebfb
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-17 17:39:18 -0300

    Exception handling - findById

--- src/main/java/com/tecsoluction/course/service/UserService.java
+++ src/main/java/com/tecsoluction/course/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.tecsoluction.course.entities.User;
 import com.tecsoluction.course.repositories.UserRepository;
+import com.tecsoluction.course.service.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
@@ -43,4 +44,5 @@ public class UserService {
         entity.setEmail(user.getEmail());
         entity.setPhone(user.getPhone());
     }
+
 }
