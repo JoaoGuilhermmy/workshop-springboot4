9bfa092eafe58f94f050b7c616b0becf8fb6d9f7
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-31 18:39:21 -0300

    Implemented post, put and delete for order

--- src/main/java/com/tecsoluction/course/service/OrderService.java
+++ src/main/java/com/tecsoluction/course/service/OrderService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.tecsoluction.course.entities.Order;
-
+import com.tecsoluction.course.entities.User;
 import com.tecsoluction.course.repositories.OrderRepository;
+import com.tecsoluction.course.service.exception.DatabaseException;
+import com.tecsoluction.course.service.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class OrderService {
@@ -22,5 +27,38 @@ public class OrderService {
     public Order findById(Long id) {
         Optional<Order> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Order insert(Order obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Order update(Long id, Order order) {
+        try {
+            Order entity = repository.getReferenceById(id);
+            updateData(entity, order);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Order entity, Order order) {
+        entity.setMoment(order.getMoment());
+        entity.setOrderStatus(order.getOrderStatus());
+        entity.setClient(order.getClient());
+        entity.setPayment(order.getPayment());
     }
 }
