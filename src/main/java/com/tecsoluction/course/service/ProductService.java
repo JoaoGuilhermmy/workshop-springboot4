2597d185e81b77c053d524c976c2f2ed35e22420
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-31 18:29:52 -0300

    implemented post, put and delete for product

--- src/main/java/com/tecsoluction/course/service/ProductService.java
+++ src/main/java/com/tecsoluction/course/service/ProductService.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.tecsoluction.course.entities.Product;
 import com.tecsoluction.course.repositories.ProductRepository;
+import com.tecsoluction.course.service.exception.DatabaseException;
+import com.tecsoluction.course.service.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,5 +26,38 @@ public class ProductService {
     public Product findById(Long id) {
         Optional<Product> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Product insert(Product obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product product) {
+        try {
+            Product entity = repository.getReferenceById(id);
+            updateData(entity, product);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product entity, Product product) {
+        entity.setName(product.getName());
+        entity.setDescprition(product.getDescprition());
+        entity.setPrice(product.getPrice());
+        entity.setImgUrl(product.getImgUrl());
     }
 }
